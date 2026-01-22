14529999155430953d9c4650760522d781ab8626
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-22 20:56:02 +0200

    adding image and its properties

--- FlashCards/ContentView.swift
+++ FlashCards/ContentView.swift
@@ -18,11 +18,15 @@ struct ContentView: View {
     @State private var cards = Array<Card>(repeating: .example, count: 10)
     var body: some View {
         ZStack{
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             VStack{
                 ZStack{
                     ForEach(0..<cards.count, id: \.self) { index in
                        CardView(card: cards[index])
                             .stacked(at: index, in: cards.count)
+                            
                     }
                 }
             }
