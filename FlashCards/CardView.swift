62998e5a7cc916a7b754e7c5ce99dfaaf6cd9b22
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-22 20:38:19 +0200

    hide answer and call it with tap gesture

--- FlashCards/CardView.swift
+++ FlashCards/CardView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct CardView: View {
+    @State private var isShowingAnswer: Bool = false
     let card: Card
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
@@ -18,15 +20,20 @@ struct CardView: View {
                 Text(card.prompt)
                     .font(.largeTitle)
                     .foregroundStyle(.black)
-                Text(card.answer)
-                    .font(.title)
-                    .foregroundStyle(.secondary)
+                if isShowingAnswer {
+                    Text(card.answer)
+                        .font(.title)
+                        .foregroundStyle(.secondary)
+                }
             }
             .padding(20)
             .multilineTextAlignment(TextAlignment.center)
                 
         }
         .frame(width: 450, height: 250)
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
     }
 }
 
