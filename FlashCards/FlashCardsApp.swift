b69620385d442f6df3b03c01982806bb8e98979d
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-22 20:17:43 +0200

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Ihor Sukhachov on 22.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
