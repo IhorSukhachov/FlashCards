c06e66593978306f86014484aeee4b6d888a73a6
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-22 20:27:18 +0200

    new card view

---
+++ FlashCards/CardView.swift
@@ -0,0 +1,19 @@
+//
+//  CardView.swift
+//  FlashCards
+//
+//  Created by Ihor Sukhachov on 22.01.2026.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView(card: .example)
+}
