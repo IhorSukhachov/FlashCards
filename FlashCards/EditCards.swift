c3b606d7100618bd745fa0f956cc018521c337c1
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-29 13:54:50 +0200

    new edit catds view

---
+++ FlashCards/EditCards.swift
@@ -0,0 +1,18 @@
+//
+//  EditCards.swift
+//  FlashCards
+//
+//  Created by Ihor Sukhachov on 29.01.2026.
+//
+
+import SwiftUI
+
+struct EditCards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCards()
+}
