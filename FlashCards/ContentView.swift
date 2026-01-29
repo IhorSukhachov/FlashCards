bc788a05efd0ea9be8c73d2309a618a560e4d980
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-29 13:14:45 +0200

    allowing hit testing only on current card

--- FlashCards/ContentView.swift
+++ FlashCards/ContentView.swift
@@ -45,6 +45,7 @@ struct ContentView: View {
                             }
                         }
                             .stacked(at: index, in: cards.count)
+                            .allowsHitTesting(index == cards.count - 1)
                             
                     }
                 }
