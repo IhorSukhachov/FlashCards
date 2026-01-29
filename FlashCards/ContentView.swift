97113246ab037d26b2f9c115ec15f1186147cdd0
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-29 13:21:34 +0200

    fix the condition

--- FlashCards/ContentView.swift
+++ FlashCards/ContentView.swift
@@ -46,7 +46,7 @@ struct ContentView: View {
                         }
                             .stacked(at: index, in: cards.count)
                             .allowsHitTesting(index == cards.count - 1)
-                            .accessibilityHidden(index < cards.count)
+                            .accessibilityHidden(index < cards.count - 1)
                             
                     }
                 }
